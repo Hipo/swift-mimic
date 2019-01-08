5ee7871b616e3a1c961b18fa9a8f888d5bb9cb43
sakkaras <sakkaras.developer@gmail.com>
2019-01-08 15:22:37 +0300

    Refactoring
    Default location of the mock files is changed.

--- Mimic/Classes/Mocking/MIMMockFinder.swift
+++ Mimic/Classes/Mocking/MIMMockFinder.swift
@@ -9,10 +9,13 @@ import Foundation
 
 open class MIMMockFinder {
     public static var mainDirectoryUrl: URL {
-        return Bundle.main.bundleURL.appendingPathComponent(
-            "Mocks",
-            isDirectory: true
-        )
+        guard let url = Bundle.main.url(
+            forResource: "Mocks",
+            withExtension: "bundle"
+        ) else {
+            fatalError("Mocks not found.")
+        }
+        return url
     }
 }
 
